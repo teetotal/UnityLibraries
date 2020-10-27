feadb13b1480ee523d1c5249bdf8e8ac6ddea713
Jung <daejung@APSEONOTE467.ad.ea.com>
2020-10-27 18:00:00 +0900

    add size functions

--- Physics2D/positions.cs
+++ Physics2D/positions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -85,5 +87,71 @@ public class Positions
         pos.y = yPre + (yNext - yPre) * 0.5f;
 
         return pos;
+    }
+    public List<Vector2> GetGridPoints(Vector2 margin, Vector2Int count)
+    {
+        return GetGridPoints(margin.x, margin.y, count.x, count.y);
+    }
+    public List<Vector2> GetGridPoints(float marginX = 0.0f, float marginY = 0.0f, int countX = 5, int countY = 5)
+    {
+        List<Vector2> list = new List<Vector2>(countX * countY);
+
+        Vector2 min = GetWorldPointMin(marginX, marginY);
+        Vector2 max = GetWorldPointMax(marginX, marginY);
+        Vector2 size = GetWorldPointSize(marginX, marginY);
+
+        float diffX = size.x / (float)countX;
+        float diffY = size.y / (float)countY;
+
+        for(int y = 0; y < countY; y++)
+        {
+            for(int x = 0; x < countX; x++)
+            {
+                Vector2 pos = new Vector2();
+                float xPre = min.x + (x * diffX);
+                float xNext = min.x + ((x+1) * diffX);
+
+                float yPre = min.y + (y * diffY);
+                float yNext = min.y + ((y+1) * diffY);
+
+                pos.x = xPre + (xNext - xPre) * 0.5f;
+                pos.y = yPre + (yNext - yPre) * 0.5f;
+
+                list.Add(pos);
+            }
+        }
+
+        return list;
+    }
+    public Vector2 GetGridSize(Vector2 margin, Vector2Int count)
+    {
+        return GetGridSize(margin.x, margin.y, count.x, count.y);
+    }
+    public Vector2 GetGridSize(float marginX = 0.0f, float marginY = 0.0f, int countX = 5, int countY = 5)
+    {
+        Vector2 size = GetWorldPointSize(marginX, marginY);
+
+        float diffX = size.x / (float)countX;
+        float diffY = size.y / (float)countY;
+
+        Vector2 diff = new Vector2(diffX, diffY);
+
+        return diff;
+    }
+
+    public Vector3 GetGameObjectSize(GameObject obj)
+    {
+        return obj.GetComponent<SpriteRenderer>().bounds.size;
+    }
+
+    public void SetGameObjectSize(ref GameObject obj, float width, float height)
+    {
+        Vector3 size = GetGameObjectSize(obj);
+        Vector3 scale = obj.transform.localScale;
+
+        float scaleX = width * scale.x / size.x;
+        float scaleY = height * scale.y / size.y;
+
+        obj.transform.localScale = new Vector3(scaleX, scaleY, scale.z);
     }
 }
