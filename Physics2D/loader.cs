45a3c265d567bc81e1b951c9ed317baa6cf2b016
Jung <daejung@APSEONOTE467.ad.ea.com>
2020-11-06 16:42:37 +0900

    add function as AddComponents

--- Physics2D/loader.cs
+++ Physics2D/loader.cs
@@ -1,8 +1,13 @@
+/*
+
+    Json파일을 읽어서 동적으로 object를 add하는 class
+
+*/
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public delegate GameObject LoaderCallBack(int id);
+public delegate GameObject LoaderCallBack(string name, string tag, Vector2 position, Vector2 size);
 [Serializable]
 public class Loader
 {
@@ -10,12 +15,15 @@ public class Loader
     public struct LoaderObject
     {
         public Vector2Int position;
-        public int id;
+        public string name;
+        public string tag;
     };
     public Vector2Int _GridDim;
     public Vector2 _Margin;
     public Vector2 _InnerMargin;
     public List<LoaderObject> _Objects = new List<LoaderObject>();
+    protected List<GameObject> mObjList = new List<GameObject>(); //생성한 object 저장용
+    protected Positions mPos = new Positions();
 
     private static readonly Lazy<Loader> hInstance =
         new Lazy<Loader>(() => new Loader());
@@ -36,6 +44,7 @@ public class Loader
     {
         Instance._GridDim = new Vector2Int(obj._GridDim.x, obj._GridDim.y);
         Instance._Margin = new Vector2(obj._Margin.x, obj._Margin.y);
+        Instance._InnerMargin = new Vector2(obj._InnerMargin.x, obj._InnerMargin.y);
         Instance._Objects = new List<LoaderObject>(obj._Objects);
     }
 
@@ -52,8 +61,24 @@ public class Loader
         Json.SaveJsonFile(path, Instance);
     }
 
-    public void AddComponents(LoaderCallBack cb)
+    public void DrawGrid()
     {
+        mPos.DrawGrid(_Margin, _GridDim);
+    }
 
+    public void AddComponents(LoaderCallBack cb)
+    {   
+        List<Vector2> points = mPos.GetGridPoints(_Margin, _GridDim);
+        Vector2 gridSize = mPos.GetGridSize(_Margin, _InnerMargin, _GridDim);
+
+        for(int n = 0; n < _Objects.Count; n++)
+        {
+            int idx = (_Objects[n].position.y *  _GridDim.x) + _Objects[n].position.x;
+            if(idx > points.Count)
+                throw new Exception("Invalid index. check _GridDim & _Objects position");
+
+            GameObject obj = cb(_Objects[n].name, _Objects[n].tag, points[idx], gridSize);
+            mObjList.Add(UnityEngine.Object.Instantiate(obj));
+        }
     }
 }
