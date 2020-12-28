56ae6d24c6b59831b326fe04b78c43671a22894f
Jung <daejung@APSEONOTE467.ad.ea.com>
2020-12-28 15:52:46 +0900

    fix Resources bug

--- json.cs
+++ json.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Json
 {
-    static string _res = Application.dataPath + "/Resources/";
+    static string _res = Application.dataPath + "Assets/Resources/";
     public static string ToJson(object json)
     {
         return JsonUtility.ToJson(json);
@@ -17,17 +17,21 @@ public class Json
 
     public static T LoadJsonFile<T>(string path)
     {
+        TextAsset buffer = Resources.Load<TextAsset>(path);
+        /*
         FileStream f = new FileStream(_res + path, FileMode.Open);
         byte[] buffer = new byte[f.Length];
         f.Read(buffer, 0, buffer.Length);
         f.Close();
-
-        string json = Encoding.UTF8.GetString(buffer);
+        */
+        
+        string json = Encoding.UTF8.GetString(buffer.bytes);
         return ToObject<T>(json);
     }
 
     public static void SaveJsonFile(string path, object json)
     {
+        //resources에 저장 못함. 저장하는법 찾아서 고쳐야 함
         string j = ToJson(json);
         FileStream f = new FileStream(_res + path, FileMode.Create);
         byte[] buffer = Encoding.UTF8.GetBytes(j);
